fa60d755bcb50134a6d87cdcd67eedfe60d81421
Wechiko <wechikosebenesi@virgilio.it>
2021-05-14 00:54:03 +0200

    add: useless update

---
+++ resources/views/posts/update.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<!-- HEADER -->
+<header class="header"> <!-- Header per contenuto di navigazione -->
+    <nav class="header__nav"> <!-- lista di link -->
+        <h1>Posts update</h1>
+    </nav>
+</header>
+
+<!-- CONTENT -->
+<div class="content">
+    <form action="{{ route('posts.edit', $post->id)}}" method="POST">
+        @csrf
+        @method('PUT')
+        Lista posts
+        <div class="form-group">
+            <label for="title">Title:</label>
+            <input  type="text" class="form-control"  id="title" name="title">
+        </div>
+
+        <div class="form-group">
+            <label for="text">Text:</label>
+            <input type="text" class="form-control" id="text" name="text">
+        </div>
+        <div class="form-group">
+            <button style="cursor:pointer" type="submit" class="btn btn-primary" >Submit</button>
+        </div>
+
+    </form>
+</div>
+</body>
+
+</html>
+
+<style>
+
+</style>
